bda7cf1e50ba38a32504aa9923115112ebc67b3a
Rahul Pal <usefulltech1819@gmail.com>
2026-02-17 22:19:09 +0530

    Add Get Notes Type

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,22 @@
 import NotesClient from "@/components/NotesClient";
 import { connectDB } from "@/lib/db";
+import { NotesModel } from "@/models/notes.model";
+
+async function getNotes() {
+  await connectDB();
+  const notes = await NotesModel.find().sort({ createdAt: -1 }).lean();
+  return notes.map((note) => ({
+    ...note,
+    _id: note?._id.toString()
+  }));
+};
 
 export default async function Home() {
-  await connectDB();
+  const notes = await getNotes();
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-6">Notes App</h1>
-      <NotesClient />
+      <NotesClient initialNotes={notes} />
     </div>
   );
 }
